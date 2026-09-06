ce972d7a492c1c55e1cc5de7038f9ba8217a5371
Alexander Trakhimenok <alex@trakhimenok.com>
2026-09-07 00:13:50 +0100

    feat: expose source linked date task reader

--- libs/calendarius/src/index.ts
+++ libs/calendarius/src/index.ts
@@ -4,3 +4,4 @@ export * from './view-models';
 export * from './schedule-nav-service';
 export * from './financial-occurrence-service';
 export * from './source-linked-date-task-navigation';
+export * from './source-linked-date-task-reader';
